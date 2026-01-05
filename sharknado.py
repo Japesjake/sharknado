ffc34e0cffd00a2bf7c97c8ec8d9defabe82ab77
John Paul Jaquith <japesjake@gmail.com>
2026-01-04 23:48:21 -0500

    added bounch mechanics for tornadoes

--- sharknado.py
+++ sharknado.py
@@ -11,10 +11,13 @@ SPAWN_PERSON_EVENT = pg.USEREVENT + 1
 SPAWN_TORNADO_EVENT = pg.USEREVENT + 2
 pg.time.set_timer(SPAWN_PERSON_EVENT, 1000)
 pg.time.set_timer(SPAWN_TORNADO_EVENT, 1000)
-
+font = pg.font.SysFont(None, 50)
+power = 0
 class Hitbox(pg.sprite.Sprite):
     def __init__(self,width,height,color,x,y,speed):
         super().__init__()
+        self.width = width
+        self.height = height
         self.image = pg.Surface([width,height])
         self.image.fill(color)
         self.rect = self.image.get_rect()
@@ -45,12 +48,21 @@ class Person(Hitbox):
 class Tornado(Hitbox):
     def __init__(self,width,height,color,x,y,speed):
         super().__init__(width,height,color,x,y,speed)
-
-class Bullet(Hitbox):
-    def __init__(self,width,height,color,x,y,speed):
-        super().__init__(width,height,color,x,y,speed)
+        self.speedX = 10
+        self.speedY = 10
     def update(self):
-        self.rect.y -= self.speed
+        if self.rect.x > screen_width - self.width or self.rect.x < 0:
+            self.speedX = -self.speedX
+        if self.rect.y > screen_height - self.height or self.rect.y < 0:
+            self.speedY = -self.speedY
+        self.rect.x += self.speedX
+        self.rect.y -= self.speedY
+
+# class Bullet(Hitbox):
+#     def __init__(self,width,height,color,x,y,speed):
+#         super().__init__(width,height,color,x,y,speed)
+#     def update(self):
+#         self.rect.y -= self.speed
 
 sharks = pg.sprite.Group()
 people = pg.sprite.Group()
@@ -61,17 +73,24 @@ sharks.add(shark_hitbox)
 
 running = True
 while running:
-    pg.sprite.groupcollide(sharks, tornadoes, False, True)
-    pg.sprite.groupcollide(bullets,people,False, True)
+    if pg.sprite.groupcollide(sharks, tornadoes, False, True): power += 1
+    if pg.sprite.groupcollide(sharks,people,False, True): power -= 1
     sharks.update()
     people.update()
     tornadoes.update()
-    bullets.update()
+    # bullets.update()
     screen.fill((0,0,0))
     sharks.draw(screen)
     people.draw(screen)
     tornadoes.draw(screen)
-    bullets.draw(screen)
+    # bullets.draw(screen)
+
+    power_text = str(power)
+    text_surface = font.render(power_text, True, (255,255,255),(0,0,0))
+    text_rect = text_surface.get_rect()
+    text_rect.x = (0)
+    text_rect.y = (0)
+    screen.blit(text_surface, text_rect)
     
 
     for event in pg.event.get():
@@ -80,10 +99,10 @@ while running:
         elif event.type == SPAWN_PERSON_EVENT:
             people.add(Person(50,50,(0,255,0),rand.randint(0,screen_width-100),0,1))
         elif event.type == SPAWN_TORNADO_EVENT:
-            tornadoes.add(Tornado(50,50,(0,0,255),rand.randint(0,screen_width-100),rand.randint(0,screen_height),1))
-        elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_SPACE:
-                bullets.add(Bullet(10,30,(255,255,255),shark_hitbox.rect.x,shark_hitbox.rect.y,1))
+            tornadoes.add(Tornado(50,50,(0,0,255),rand.randint(0,screen_width-100),rand.randint(0,screen_height-100),1))
+        # elif event.type == pg.KEYDOWN:
+        #     if event.key == pg.K_SPACE:
+        #         bullets.add(Bullet(10,30,(255,255,255),shark_hitbox.rect.x,shark_hitbox.rect.y,1))
                            
     pg.display.flip()
     clock.tick(FPS)
